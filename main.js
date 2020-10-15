e33b00379190dbd0fd595d1d4db18c3f1525bbfb
nxae <72491680+nxae@users.noreply.github.com>
2020-10-15 17:45:26 +0300

    v2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /**
- * Create DNS Record Action for GitHub
- * https://github.com/marketplace/actions/create-dns-record
+ * Create Cloudflare DNS Record Action for GitHub
+ * Originally forked from https://github.com/kriasoft/create-dns-record
  */
 
 const path = require("path");
@@ -14,36 +14,98 @@ if (['false', '0', 'no'].includes(process.env.INPUT_PROXIED)) {
   shouldBeProxied = false;
 }
 
+//https://api.cloudflare.com/#dns-records-for-a-zone-list-dns-records
+const getCurrentRecordId = () => {
+  const { status, stdout } = cp.spawnSync("curl", [
+    ...["--header", `Authorization: Bearer ${process.env.INPUT_TOKEN}`],
+    ...["--header", "Content-Type: application/json"],
+    `https://api.cloudflare.com/client/v4/zones/${process.env.INPUT_ZONE}/dns_records`,
+  ]);
+  const { success, result, errors } = JSON.parse(stdout.toString());
+  const name = process.env.INPUT_NAME;
+  const record = result.find((x) => x.name === name);
+
+  if (status !== 0) {
+    process.exit(status);
+  }
+  if (!success) {
+    console.log(`::error ::${errors[0].message}`);
+    process.exit(1);
+  }
+  if (!record) {
+    return null
+  }
+  return record.id;
+};
+
 // https://api.cloudflare.com/#dns-records-for-a-zone-create-dns-record
-const curlResult = cp.spawnSync("curl", [
-  ...["--request", "POST"],
-  ...["--header", `Authorization: Bearer ${process.env.INPUT_TOKEN}`],
-  ...["--header", "Content-Type: application/json"],
-  ...["--silent", "--data"],
-  JSON.stringify({
-    type: process.env.INPUT_TYPE,
-    name: process.env.INPUT_NAME.replace(/\{pr\}/gi, pr)
-      .replace(/\{pr_number\}/gi, pr)
-      .replace(/\{head_ref\}/gi, process.env.GITHUB_HEAD_REF),
-    content: process.env.INPUT_CONTENT,
-    ttl: Number(process.env.INPUT_TTL),
-    proxied: shouldBeProxied,
-  }),
-  `https://api.cloudflare.com/client/v4/zones/${process.env.INPUT_ZONE_ID}/dns_records`,
-]);
+const createRecord = () => {
+  const { status, stdout } = cp.spawnSync("curl", [
+    ...["--request", "POST"],
+    ...["--header", `Authorization: Bearer ${process.env.INPUT_TOKEN}`],
+    ...["--header", "Content-Type: application/json"],
+    ...["--silent", "--data"],
+    JSON.stringify({
+      type: process.env.INPUT_TYPE,
+      name: process.env.INPUT_NAME.replace(/\{pr\}/gi, pr)
+        .replace(/\{pr_number\}/gi, pr)
+        .replace(/\{head_ref\}/gi, process.env.GITHUB_HEAD_REF),
+      content: process.env.INPUT_CONTENT,
+      ttl: Number(process.env.INPUT_TTL),
+      proxied: shouldBeProxied,
+    }),
+    `https://api.cloudflare.com/client/v4/zones/${process.env.INPUT_ZONE}/dns_records`,
+  ]);
+  const { success, result, errors } = JSON.parse(stdout.toString());
 
-if (curlResult.status !== 0) {
-  process.exit(curlResult.status);
+  if (status !== 0) {
+    process.exit(status);
+  }
+  if (!success) {
+    console.dir(errors[0]);
+    console.log(`::error ::${errors[0].message}`);
+    process.exit(1);
+  }
+  console.log(`::set-output name=id::${result.id}`);
+  console.log(`::set-output name=name::${result.name}`);
+};
+
+//https://api.cloudflare.com/#dns-records-for-a-zone-update-dns-record
+const updateRecord = (id) => {
+  console.log(`Record exists with ${id}, updating...`);
+  const { status, stdout } = cp.spawnSync("curl", [
+    ...["--request", "PUT"],
+    ...["--header", `Authorization: Bearer ${process.env.INPUT_TOKEN}`],
+    ...["--header", "Content-Type: application/json"],
+    ...["--silent", "--data"],
+    JSON.stringify({
+      type: process.env.INPUT_TYPE,
+      name: process.env.INPUT_NAME.replace(/\{pr\}/gi, pr)
+        .replace(/\{pr_number\}/gi, pr)
+        .replace(/\{head_ref\}/gi, process.env.GITHUB_HEAD_REF),
+      content: process.env.INPUT_CONTENT,
+      ttl: Number(process.env.INPUT_TTL),
+      proxied: shouldBeProxied,
+    }),
+    `https://api.cloudflare.com/client/v4/zones/${process.env.INPUT_ZONE}/dns_records/${id}`,
+  ]);
+  const { success, result, errors } = JSON.parse(stdout.toString());
+
+  if (status !== 0) {
+    process.exit(status);
+  }
+  if (!success) {
+    console.dir(errors[0]);
+    console.log(`::error ::${errors[0].message}`);
+    process.exit(1);
+  }
+  console.log(`::set-output name=record_id::${result.id}`);
+  console.log(`::set-output name=name::${result.name}`);
 }
 
-const { success, result, errors } = JSON.parse(curlResult.stdout.toString());
-
-if (!success) {
-  console.dir(errors[0]);
-  console.log(`::error ::${errors[0].message}`);
-  process.exit(1);
+const id = getCurrentRecordId();
+if (id) {
+  updateRecord(id);
+  process.exit(0);
 }
-
-console.dir(result);
-console.log(`::set-output name=id::${result.id}`);
-console.log(`::set-output name=name::${result.name}`);
+createRecord();
